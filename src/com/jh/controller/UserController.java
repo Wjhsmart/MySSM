fc433a4fd3837ccd521a9f4d45b6f22196b6c0f3
Wjhsmart <672630243@qq.com>
2017-02-18 08:36:48 +0800

    使用logback
    在controller中使用logback

--- src/com/jh/controller/UserController.java
+++ src/com/jh/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.jh.bean.User;
 import com.jh.service.UserService;
 
+import ch.qos.logback.classic.Logger;
+
 @Controller
 @RequestMapping("user")
 public class UserController {
@@ -19,9 +22,12 @@ public class UserController {
 	@Resource
 	private UserService userService;
 	
+	private Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
+	
 	@ResponseBody
 	@RequestMapping(value="all", method=RequestMethod.GET)
 	public List<User> queryAll() {
+		logger.info("查询所有用户信息");
 		return userService.queryAll();
 	}
 }
